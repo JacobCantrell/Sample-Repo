07792a2ff56231c9e46e486d370e0d862bb7710d
jhcantrell <jhcantrell@outlook.com>
2020-08-12 21:28:10 -0700

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import sys
+
+print (sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'hello, {}'.format(who_to_greet)
+    return greeting
+
+
+print(greet('world'))
+print(greet('Jacob'))
+
+print('jacob')
+
